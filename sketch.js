b2422d51928f9450ba3017c90e3118204e680ad0
AkshVerma2501 <74178683+AkshVerma2501@users.noreply.github.com>
2020-11-25 15:03:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,box2,box3,box4,box5;
+var pig1,pig2;
+var log1,log2,log3,log4;
+
+function setup(){
+    var canvas = createCanvas(1800,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(600,300,55,55);
+    box2 = new Box(800,300,55,55);
+    box3 = new Box(600,270,55,55);
+    box4 = new Box(800,270,55,55);
+    box5 = new Box(700,200,55,55);
+    
+    pig1 = new pig(700,295);
+    pig2 = new pig(700,270);
+
+    log1 = new Log(700,280,270,PI/2);
+    log2 = new Log(700,250,270,PI/2);
+    log3 = new Log(650,200,170,PI/7);
+    log4 = new Log(750,200,170,-PI/7);
+
+
+
+
+
+    ground = new Ground(900,height,1800,20)
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    
+    ground.display();
+
+    pig1.display();
+    pig2.display();
+    
+    log1.display();
+    log2.display();
+    log3.display();
+    log4.display();
+
+}
